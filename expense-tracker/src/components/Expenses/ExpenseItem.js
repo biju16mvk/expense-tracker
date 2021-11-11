692579bab10c5a98dd0da6a2f2d17106124a4860
Biju Kattiyil Murali <biju.murali@metro-markets.de>
2021-11-11 07:10:52 +0100

    Dollar sign added for amount

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -9,7 +9,7 @@ const ExpenseItem = props => {
           <ExpenseDate date={expense.date}/>
           <div className="expense-item__description">
             <h2>{expense.title}</h2>
-            <div className="expense-item__price">{expense.amount}</div>
+            <div className="expense-item__price">${expense.amount}</div>
           </div>
       </Card>
       ))
